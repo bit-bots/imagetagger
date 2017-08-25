7869bd378d4e4b5e346e0c8e0ac676476b7aea32
Pascal Wichmann <git@pa-w.de>
2017-08-25 19:34:26 +0200

    Clean up annotation javascript

--- imagetagger/imagetagger/annotations/static/annotations/js/annotate.js
+++ imagetagger/imagetagger/annotations/static/annotations/js/annotate.js
@@ -1,153 +1,199 @@
-var selection;
-var image_scale = 0;
-
-$(document).ready(function() {
+(function() {
+  var image;
+  var imageScale = 0;
+  var initialized = false;
+  var mousepos;
   var selection;
-  var image_scale = 0;
-  var image = $('img#picture');
 
-    $(function () {
+  /**
+   * Calculate the correct imageScale value.
+   */
+  function calculateImageScale() {
+    imageScale = image.get(0).naturalWidth / image.width();
+  }
 
-      /*Setzt den Status auf verifiziert wenn das Objekt als nicht im Bild getagt ist*/
-      $("#not_in_image").each(function(){
-          if ($(this).is(":checked"))
-              ReturnVal = true;
-      });
+  /**
+   * Handle a selection using the mouse.
+   *
+   * @param event
+   */
+  function handleSelection(event) {
+    calculateImageScale();
+    var cH = $('#crosshair-h'), cV = $('#crosshair-v');
+    var position = image.offset();
+    if (event.pageX > position.left &&
+          event.pageX < position.left + image.width() &&
+          event.pageY > position.top &&
+          event.pageY < position.top + image.height()) {
+      cH.show();
+      cV.show();
+      mousepos.show();
 
+      cH.css('top', event.pageY + 1);
+      cV.css('left', event.pageX + 1);
+      cV.css('height', image.height() - 1);
+      cV.css('margin-top', position.top);
+      cH.css('width', image.width() - 1);
+      cH.css('margin-left', position.left);
 
-      /*wenn content geladen setze die variable neu und wenn image_scale nicht größer 0 ist setze die globale variable neu und aktualisiere image_scale*/
-      $( function() {
+      mousepos.css({
+        top: (event.pageY) + 'px',
+        left: (event.pageX)  + 'px'
+      }, 800);
+      mousepos.text(
+        '(' + Math.round((event.pageX - position.left) * imageScale) + ', ' +
+        Math.round((event.pageY - position.top) * imageScale) + ')');
+      event.stopPropagation();
+    }
+    else{
+      cH.hide();
+      cV.hide();
+      mousepos.hide();
+    }
+  }
 
-          selection = image.imgAreaSelect({
-              instance: true,
-              handles: false,
-              minHeight: 2,
-              minWidth: 2,
-              onSelectChange: function (img, selection) {
-                  $('#x1Field').val(Math.ceil(selection.x1 * image_scale));
-                  $('#y1Field').val(Math.ceil(selection.y1 * image_scale));
-                  $('#x2Field').val(Math.floor(selection.x2 * image_scale));
-                  $('#y2Field').val(Math.floor(selection.y2 * image_scale));
-              }
-          });
+  /**
+   * Handle a resize event of the window.
+   *
+   * @param event
+   */
+  function handleResize() {
+    selection.cancelSelection();
+    calculateImageScale();
+  }
 
-          /*läd annotations*/
-          if (typeof loadannotation === "function") { //tageditview
-            loadannotation();
-            reload_selection();
-          }
-      });
+  /**
+   * Initialize the selection.
+   */
+  function initSelection() {
+    initialized = true;
 
-      $('#x1Field')[0].oninput = function () {
-          reload_selection();
-      };
-      $('#y1Field')[0].oninput = function () {
-          reload_selection();
-      };
-      $('#x2Field')[0].oninput = function () {
-          reload_selection();
-      };
-      $('#y2Field')[0].oninput = function () {
-          reload_selection();
-      };
+    selection = image.imgAreaSelect({
+      instance: true,
+      show: true,
+      minHeight: 2,
+      minWidth: 2,
+      onSelectChange: updateAnnotationFields
+    });
+    selection.cancelSelection();
+  }
 
-      $('#reset_button').click(reset_selection);
+  /**
+   * Reload the selection.
+   */
+  function reloadSelection() {
+    selection = image.imgAreaSelect({
+      instance: true,
+      show: true
+    });
+    selection.setSelection(
+      Math.round($('#x1Field').val() / imageScale),
+      Math.round($('#y1Field').val() / imageScale),
+      Math.round($('#x2Field').val() / imageScale),
+      Math.round($('#y2Field').val() / imageScale)
+    );
+    selection.update();
+  }
 
-      $(this).on('mousemove touchmove', function(e) {
-          var cH = $('#crosshair-h'), cV = $('#crosshair-v');
-          image = $('img#picture');
-          var position = image.offset();
-          if((e.pageX > position.left && e.pageX < (position.left + image.width())) && (e.pageY > position.top && e.pageY < (position.top + image.height()))){
-              cH.show();
-              cV.show();
-              $('#mousepos').show();
+  /**
+   * Delete current selection.
+   */
+  function resetSelection() {
+    $('.annotation_value').val(0);
 
-              cH.css('top', e.pageY +1);
-              cV.css('left', e.pageX +1);
-              cV.css("height", image.height() -1);
-              cV.css("margin-top", position.top);
-              cH.css("width", image.width() -1);
-              cH.css("margin-left", position.left);
-              if(!image_scale > 0) {
-                  var image = $('img#picture');
-                  image_scale =  image.get(0).naturalWidth / image.width();
-              }
+    selection.cancelSelection();
+  }
 
-              $('#mousepos').css({
-                  top: (e.pageY) + 'px',
-                  left: (e.pageX)  + 'px'
-              }, 800);
-              $('#mousepos').text( "(" + Math.round((e.pageX - position.left)*image_scale) + ", " + Math.round((e.pageY - position.top)*image_scale) + ")");
-              e.stopPropagation();
-          }
-          else{
-              cH.hide();
-              cV.hide();
-              $('#mousepos').hide();
-          }
-      });
+  /**
+   * Update the contents of the annotation values
+   *
+   * @param img
+   * @param selection
+   */
+  function updateAnnotationFields(img, selection) {
+    $('#x1Field').val(Math.ceil(selection.x1 * imageScale));
+    $('#y1Field').val(Math.ceil(selection.y1 * imageScale));
+    $('#x2Field').val(Math.floor(selection.x2 * imageScale));
+    $('#y2Field').val(Math.floor(selection.y2 * imageScale));
+  }
 
-      $('#mousepos').hide();
+  $(function() {
+    image = $('#image');
+    mousepos = $('#mousepos');
+    mousepos.hide();
 
-      if (typeof init_navigationbuttons === "function") { //tagview
-        init_navigationbuttons();
+    // W3C standards do not define the load event on images, we therefore need to use
+    // it from window (this should wait for all external sources including images)
+    $(window).on('load', initSelection);
 
-          $(document).keyup(function(event){
-              switch(event.keyCode){
-                  case 70: //f
-                      $('#next_button').click();
-                      break;
-                  case 68: //d
-                      $('#skip_button').click();
-                      break;
-                  case 83: //s
-                      $('#back_button').click();
-                      break;
-                  case 65: //a
-                      $('#last_button').click();
-                      break;
-                  case 71: //g
-                      var nii = $('#not_in_image');
-                      if(nii.prop('checked')) {
-                          nii.prop('checked', false)
-                      }
-                      else {
-                          nii.prop('checked', true)
-                      }
-                      break;
-                  case 82: //r
-                      $('#reset_button').click();
-                      break;
-                  case 86: //'v'
-                      $('#annotation_form').submit();
-                      break;
-              }
-          });
-
+    setTimeout(function() {
+      // Fallback if window load initialization did not succeed
+      if (!initialized) {
+        console.log('fallback solution for selection initialization used!');
+        initSelection();
+        // TODO: Get rid of this
+        // This is used to load an existing annotation in the edit view
+        if (typeof loadannotation === "function") {
+          loadannotation();
+          reloadSelection();
+        }
       }
+    }, 1000);
+
+    $('.annotation_value').on('input', function() {
+      reloadSelection();
+    });
+
+    $('#reset_button').click(resetSelection);
+
+    $(document).on('mousemove touchmove', handleSelection);
+    $(window).on('resize', handleResize);
+
+
+    // TODO: this should be done only for the annotate view
+    $(document).keyup(function(event) {
+      switch (event.keyCode){
+        case 70: //f
+          $('#next_button').click();
+          break;
+        case 68: //d
+          $('#skip_button').click();
+          break;
+        case 83: //s
+          $('#back_button').click();
+          break;
+        case 65: //a
+          $('#last_button').click();
+          break;
+        case 71: //g
+          var nii = $('#not_in_image');
+          if(nii.prop('checked')) {
+            nii.prop('checked', false)
+          }
+          else {
+            nii.prop('checked', true)
+          }
+          break;
+        case 82: //r
+          $('#reset_button').click();
+          break;
+        case 86: //'v'
+          $('#annotation_form').submit();
+          break;
+      }
+    });
+
+    // TODO: Get rid of this
+    // This is used to load an existing annotation in the edit view
+    if (typeof loadannotation === "function") {
+      loadannotation();
+      reloadSelection();
+    }
+
+    // TODO: Get rid of this
+    if (typeof init_navigationbuttons === "function") {
+      init_navigationbuttons();
+      reloadSelection();
+    }
   });
-
-
-  //Änderung der Fenstergröße hebt Selektion auf und berechnet scale neu
-  $(window).resize(function() {
-      selection.cancelSelection();
-      image_scale = $('img#picture').get(0).naturalWidth / $('img#picture').width();
-  });
-
-  //Wiederherstellung der Selektion
-  function reload_selection() {
-      selection = $('img#picture').imgAreaSelect({ instance: true, show: true });
-      selection.setSelection(Math.round($('#x1Field').val() / image_scale), Math.round($('#y1Field').val() / image_scale), Math.round($('#x2Field').val() / image_scale), Math.round($('#y2Field').val() / image_scale));
-      selection.update();
-  }
-
-  //Löschen der Selektion
-  function reset_selection() {
-      $('#x1Field').val(0);
-      $('#y1Field').val(0);
-      $('#x2Field').val(0);
-      $('#y2Field').val(0);
-      selection.cancelSelection();
-  }
-
-});
+})();
