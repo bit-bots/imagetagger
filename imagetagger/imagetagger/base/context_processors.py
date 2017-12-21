b5ee365d52689b38fd8430e84db4a6f02f722130
Niklas Fiedler <git@nfiedler.de>
2017-12-21 13:33:04 +0100

    renamed base context processor

--- imagetagger/imagetagger/base/context_processors.py
+++ imagetagger/imagetagger/base/context_processors.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 
 
-def imprint_data(request):
+def base_data(request):
     return {
         'IMPRINT_URL': settings.IMPRINT_URL,
         'USE_IMPRINT': settings.USE_IMPRINT,
         'IMPRINT_NAME': settings.IMPRINT_NAME,
+        'TOOLS_ENABLED': settings.TOOLS_ENABLED,
     }
+
