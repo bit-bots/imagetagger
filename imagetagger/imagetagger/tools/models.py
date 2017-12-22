613e089af6eba188cb6e42985da13b0ac8fa6250
Niklas Fiedler <git@nfiedler.de>
2017-12-22 01:03:09 +0100

    added public to tool model

--- imagetagger/imagetagger/tools/models.py
+++ imagetagger/imagetagger/tools/models.py
@@ -8,6 +8,7 @@ class Tool(models.Model):
     filename = models.CharField(max_length=255)
     description = models.TextField()
     creation_date = models.DateTimeField(auto_now_add=True)
+    public = models.BooleanField(default=False)
     team = models.ForeignKey(Team,
                              on_delete=models.SET_NULL,
                              related_name='tool_team',
