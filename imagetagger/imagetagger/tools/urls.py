d9b333c9ec8c14153c9bec77dc901c097b6f49c2
Niklas Fiedler <git@nfiedler.de>
2017-12-22 01:00:48 +0100

    fixed tool urls

--- imagetagger/imagetagger/tools/urls.py
+++ imagetagger/imagetagger/tools/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'tools'
 urlpatterns = [
     url(r'^$', views.overview, name='overview'),
-    url(r'^/create/$', views.create_tool, name='create'),
-    url(r'^/delete/(\d+)/$', views.delete_tool, name='delete'),
-    url(r'^/edit/(\d+)/$', views.edit_tool, name='edit'),
-    url(r'^/download/(\d+)/$', views.download_tool, name='download'),
+    url(r'^create/$', views.create_tool, name='create'),
+    url(r'^delete/(\d+)/$', views.delete_tool, name='delete'),
+    url(r'^edit/(\d+)/$', views.edit_tool, name='edit'),
+    url(r'^download/(\d+)/$', views.download_tool, name='download'),
 ]
