2f32543d4cff281ccc582b0a3f6c8ea8298cdf94
Niklas Fiedler <git@nfiedler.de>
2017-12-22 03:22:49 +0100

    initial tool creation

--- imagetagger/imagetagger/tools/views.py
+++ imagetagger/imagetagger/tools/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from django.urls import reverse
 from django.http import Http404
+from django.shortcuts import redirect
+from django.db import transaction
 from django.db.models import Q
 from django.template.response import TemplateResponse
 from imagetagger.tools.models import Tool
+from imagetagger.tools.forms import ToolUploadForm
+import os
 
 
 def tools_enabled(in_function):
@@ -27,7 +32,19 @@ def overview(request):
 @tools_enabled
 @login_required
 def create_tool(request):
-    pass
+    if request.method == 'POST':
+        form = ToolUploadForm(request.POST)
+        if form.is_valid():
+            with transaction.atomic():
+                tool = form.instance.save()
+            tool.filename = '{}_{}'.format(tool.id,
+                                           request.FILES['file'].name)
+            if not os.path.isdir(settings.TOOLS_PATH):
+                os.makedirs(settings.TOOLS_PATH)
+            with open(os.path.join(settings.TOOLS_PATH, tool.filename), 'w+') as f:
+                for chunk in request.FILES['file']:
+                    f.write(chunk)
+            return redirect(reverse('tools:overview'))
 
 
 @tools_enabled
