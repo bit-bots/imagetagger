b240514715788dad41207befcbea1add448c6c4b
Niklas Fiedler <git@nfiedler.de>
2017-12-22 12:27:54 +0100

    added tools forms.py

---
+++ imagetagger/imagetagger/tools/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from imagetagger.tools.models import Tool
+from django.core.exceptions import ValidationError
+
+
+# found this under:
+# https://stackoverflow.com/questions/2472422/django-file-upload-size-limit
+def file_size(value):
+    limit = 2 * 1024 * 1024
+    if value.size > limit:
+        raise ValidationError('File too large. Size should not exceed 2 MiB.')
+
+
+class ToolUploadForm(forms.ModelForm):
+    class Meta:
+        model = Tool
+        fields = [
+            'name',
+            'description',
+            'public',
+        ]
+    file = forms.FileField(max_length=250, required=True, validators=[file_size])
+
