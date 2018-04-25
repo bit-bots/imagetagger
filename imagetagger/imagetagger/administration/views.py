2862ceb364b56f2af86eaef6568cc9a03c6010b3
Niklas Fiedler <git@nfiedler.de>
2018-04-25 21:10:46 +0200

    added vector type migration

--- imagetagger/imagetagger/administration/views.py
+++ imagetagger/imagetagger/administration/views.py
@@ -8,13 +8,13 @@ from django.contrib import messages
 from django.db import transaction
 
 from .forms import AnnotationTypeCreationForm, AnnotationTypeEditForm
-from imagetagger.annotations.models import AnnotationType
+from imagetagger.annotations.models import Annotation, AnnotationType
 
 
 @staff_member_required
 def annotation_types(request):
     return render(request, 'administration/annotation_type.html', {
-        'annotation_types': AnnotationType.objects.all(),
+        'annotation_types': AnnotationType.objects.all().order_by('name'),
         'create_form': AnnotationTypeCreationForm,
     })
 
@@ -25,6 +25,7 @@ def annotation_type(request, annotation_type_id):
     return render(request, 'administration/annotation_type.html', {
         'annotation_types': AnnotationType.objects.all().order_by('name'),
         'annotation_type': selected_annotation_type,
+        'vector_type_name': AnnotationType.get_vector_type_name(selected_annotation_type.vector_type),
         'create_form': AnnotationTypeCreationForm(),
         'edit_form': AnnotationTypeEditForm(instance=selected_annotation_type)
     })
@@ -67,3 +68,53 @@ def edit_annotation_type(request, annotation_type_id):
 
             messages.success(request, _('The annotation type was edited successfully.'))
     return redirect(reverse('administration:annotation_type', args=(annotation_type_id, )))
+
+
+@staff_member_required
+def migrate_bounding_box_to_0_polygon(request, annotation_type_id):
+    selected_annotation_type = get_object_or_404(AnnotationType, id=annotation_type_id)
+
+    if selected_annotation_type.vector_type is AnnotationType.VECTOR_TYPE.BOUNDING_BOX:
+        annotations = Annotation.objects.filter(annotation_type=selected_annotation_type)
+        for annotation in annotations:
+            annotation.verifications.all().delete()
+            if annotation.vector:
+                annotation.vector = {
+                    'x1': annotation.vector['x1'],
+                    'y1': annotation.vector['y1'],
+                    'x2': annotation.vector['x2'],
+                    'y2': annotation.vector['y1'],
+                    'x3': annotation.vector['x2'],
+                    'y3': annotation.vector['y2'],
+                    'x4': annotation.vector['x1'],
+                    'y4': annotation.vector['y2'],
+                }
+            annotation.save()
+        selected_annotation_type.vector_type = AnnotationType.VECTOR_TYPE.POLYGON
+        selected_annotation_type.node_count = 0
+        selected_annotation_type.save()
+
+
+@staff_member_required
+def migrate_bounding_box_to_4_polygon(request, annotation_type_id):
+    selected_annotation_type = get_object_or_404(AnnotationType, id=annotation_type_id)
+
+    if selected_annotation_type.vector_type is AnnotationType.VECTOR_TYPE.BOUNDING_BOX:
+        annotations = Annotation.objects.filter(annotation_type=selected_annotation_type)
+        for annotation in annotations:
+            annotation.verifications.all().delete()
+            if annotation.vector:
+                annotation.vector = {
+                    'x1': annotation.vector['x1'],
+                    'y1': annotation.vector['y1'],
+                    'x2': annotation.vector['x2'],
+                    'y2': annotation.vector['y1'],
+                    'x3': annotation.vector['x2'],
+                    'y3': annotation.vector['y2'],
+                    'x4': annotation.vector['x1'],
+                    'y4': annotation.vector['y2'],
+                }
+            annotation.save()
+        selected_annotation_type.vector_type = AnnotationType.VECTOR_TYPE.POLYGON
+        selected_annotation_type.node_count = 4
+        selected_annotation_type.save()
