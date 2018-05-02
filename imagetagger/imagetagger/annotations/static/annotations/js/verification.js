710ecdc83e42e1def6772b77425dee8ed6472d2a
Niklas Fiedler <git@nfiedler.de>
2018-05-02 10:31:49 +0200

    removed last/next unverified to simplify the UI

--- imagetagger/imagetagger/annotations/static/annotations/js/verification.js
+++ imagetagger/imagetagger/annotations/static/annotations/js/verification.js
@@ -211,17 +211,11 @@
 
     $(document).keyup(function(event){
       switch(event.keyCode){
-        case 70: //f
-          $('#next_unverified_button').click();
-          break;
         case 68: //d
           $('#next_button').click();
           break;
         case 83: //s
           $('#last_button').click();
-          break;
-        case 65: //a
-          $('#last_unverified_button').click();
           break;
         case 69: //e
           $('#edit_button').click();
