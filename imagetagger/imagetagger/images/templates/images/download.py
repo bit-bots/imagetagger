a24bd88d5a98101e70ebb56e842cf21396ab4982
5hagge <5hagge@informatik.uni-hamburg.de>
2018-05-24 15:22:54 +0200

    improved python download script

--- imagetagger/imagetagger/images/templates/images/download.py
+++ imagetagger/imagetagger/images/templates/images/download.py
@@ -1,36 +1,42 @@
+#!/usr/bin/env python3
+
 import sys
 import getpass
 import requests
-from bs4 import BeautifulSoup
-import os
 import shutil
+import os
 
 # TODO: replace imagetagger.bit-bots.de with {{ base_url }}
-# TODO: zielordner
-# TODO: mehrere imagesets auf einmal
-# TODO: -h
+
+BaseUrl = "https://imagetagger.bit-bots.de/"#{{ base_url }}"
+#BaseUrl = "http://127.0.0.1:8000/"
 if len(sys.argv) < 2:
-    imageset = input("Imageset:")
+    imageset = input("Imagesets you want to download, separated by a ',':")
 else:
-    imageset = sys.argv[1]
-print("Please enter Username and Password for the ImageTagger. This Script will download into the current directory.")
+    if sys.argv[1]  == '-h':
+        print("This script will download images from the specified imageset for you.")
+        print("The images will be downloaded from: {}".format(BaseUrl))
+        sys.exit()
+    else:
+        imageset = sys.argv[1]
 user = input("Username:")
 password = getpass.getpass()
-
+print()
+print("Enter in which relative subdirectory your images should be saved")
+filename = input("The Imagesets will be stored in a subdirectory named after their id:")
+if not os.path.exists(os.getcwd() + '/' +filename):
+    os.makedirs(os.getcwd()+'/'+filename)
+imagesets = imageset.split(',')
 
 def download_imageset(current_imageset):
-    #BaseUrl = "https://imagetagger.bit-bots.de/"#{{ base_url }}"
-    BaseUrl = "http://127.0.0.1:8000/"
+    error = False
+    if not os.path.exists("{}/{}/{}".format(os.getcwd(), filename, current_imageset)):
+        os.makedirs(os.getcwd()+'/'+filename+'/'+current_imageset)
     loginpage = requests.get(BaseUrl)
     csrftoken = loginpage.cookies['csrftoken']
-    loginsoup = BeautifulSoup(loginpage.text, 'html.parser')
-    for i in loginsoup.find_all('input'):
-        if i['name'] == 'csrfmiddlewaretoken':
-            csrfmiddlewaretoken = i['value']
-            break
 
     cookies = {'csrftoken': csrftoken}
-    #cookies = {'csrfmiddlewaretoken': csrfmiddlewaretoken}
+    csrfmiddlewaretoken = csrftoken
     data = {'username': user,
             'password': password,
             'csrfmiddlewaretoken': csrfmiddlewaretoken}
@@ -46,12 +52,13 @@ def download_imageset(current_imageset):
     except:
         print('Login failed')
         sys.exit()
-    print("Sessionid:"+sessionid)
     cookies = {'sessionid' : sessionid}
-    page = requests.get("{}images/imagelist/{}/".format(BaseUrl, imageset),cookies = cookies)
+    page = requests.get("{}images/imagelist/{}/".format(BaseUrl,
+                                                        current_imageset),
+                                                        cookies = cookies)
     images = page.text.replace('\n','')
     images = images.split(',')
-    for image in images:
+    for index,image in enumerate(images):
         if image == '':
             continue
         r = requests.get(BaseUrl+image[1:],
@@ -60,14 +67,18 @@ def download_imageset(current_imageset):
                      allow_redirects=False,
                      headers={'referer': BaseUrl},
                      stream = True)
-        print(image)
-        print(BaseUrl+image[1:])
+        if "Not Found" in r.text:
+            print("In Imageset {} was an error. The server returned page not found.".format(current_imageset))
+            error = True
+            continue
         image = image.split('?')[1]
-        print(image)
-        with open('tmp/{}'.format(image), 'wb') as f:
+        with open('{}/{}/{}'.format(filename,current_imageset,image), 'wb') as f:
             r.raw.decode_content = True
             shutil.copyfileobj(r.raw, f)
-    print('Imageset {} has been downloaded.'.format(current_imageset))
+            sys.stdout.flush()
+            print("{}Image {} / {} has been downloaded from imageset {}".format("\r",index+1,len(images)-1,current_imageset),end="")
+    if not error:
+        print('\nImageset {} has been downloaded.'.format(current_imageset))
 
-
-download_imageset(imageset)
+for imgset in imagesets:
+    download_imageset(imgset)
