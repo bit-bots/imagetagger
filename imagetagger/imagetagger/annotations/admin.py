40d743cdc63d74a7d7624075e78c1efe2586a438
Pascal Wichmann <git@pa-w.de>
2018-06-24 20:36:09 +0200

    Use raw id fields for annotation and verification foreign keys

--- imagetagger/imagetagger/annotations/admin.py
+++ imagetagger/imagetagger/annotations/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 
 from .models import Annotation, AnnotationType, Export, Verification, ExportFormat
 
-admin.site.register(Annotation)
+@admin.register(Annotation)
+class AnnotationAdmin(admin.ModelAdmin):
+    raw_id_fields = (
+        'image',
+    )
+
+
+@admin.register(Verification)
+class VerificationAdmin(admin.ModelAdmin):
+    raw_id_fields = (
+        'annotation',
+    )
+
+
 admin.site.register(AnnotationType)
 admin.site.register(Export)
-admin.site.register(Verification)
 admin.site.register(ExportFormat)
