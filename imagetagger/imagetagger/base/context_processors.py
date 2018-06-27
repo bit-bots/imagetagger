e112e80b14843bfecbe1eba31c8f2f9e79af7155
Timon Engelke <timon.engelke@online.de>
2018-06-27 11:46:40 -0400

    Add quick button for team list (closes 58)

--- imagetagger/imagetagger/base/context_processors.py
+++ imagetagger/imagetagger/base/context_processors.py
@@ -1,11 +1,18 @@
 from django.conf import settings
+from imagetagger.users.models import Team
 
 
 def base_data(request):
+    if request.user.is_authenticated:
+        my_teams = Team.objects.filter(members=request.user)
+    else:
+        my_teams = None
+
     return {
         'IMPRINT_URL': settings.IMPRINT_URL,
         'USE_IMPRINT': settings.USE_IMPRINT,
         'IMPRINT_NAME': settings.IMPRINT_NAME,
         'TOOLS_ENABLED': settings.TOOLS_ENABLED,
+        'my_teams': my_teams,
     }
 
