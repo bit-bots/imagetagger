4243d0c0b3567adfcd7b9e732ca992c082099bab
Timon Engelke <timon.engelke@online.de>
2018-08-01 19:50:43 +0200

    Remove default=None from migration

--- imagetagger/imagetagger/tagger_messages/migrations/0001_initial.py
+++ imagetagger/imagetagger/tagger_messages/migrations/0001_initial.py
@@ -47,7 +47,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='message',
             name='creator',
-            field=models.ForeignKey(default=None, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL),
         ),
         migrations.AddField(
             model_name='message',
