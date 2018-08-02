bf002f75da8146cfb4511af4d6f5ff81cc64f450
Florian <florian@flova.de>
2018-08-02 19:52:22 +0200

    Add admin messages and fixing non admin publishing

--- imagetagger/imagetagger/tagger_messages/views.py
+++ imagetagger/imagetagger/tagger_messages/views.py
@@ -4,18 +4,20 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import redirect
-
+from imagetagger.users.models import TeamMembership
 
 @login_required
 def send_team_message(request):
     if request.method == 'POST':
         form = TeamMessageCreationForm(request.POST)
-        if form.is_valid():
+        if (form.is_valid() and TeamMembership.objects.filter(
+                user=request.user, team=form.instance.team, is_admin=True
+                ).exists()):
             with transaction.atomic():
                 form.instance.creator = request.user
-                form.instance.save()  # TODO fake
-                form.instance.read_by.add(request.user)
-                form.save()
+                form.instance.save()
+                # form.instance.read_by.add(request.user)
+                #form.instance.save()
             return redirect(request.POST['source'])
         messages.error(request, 'Invalid message form')
     if 'source' in request.POST:
