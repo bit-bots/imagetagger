408a4dc7573f7ac23dde610c6c0d0e891dfe1331
Florian <florian@flova.de>
2018-08-07 13:57:06 +0200

    Adds view for global messages

--- imagetagger/imagetagger/tagger_messages/urls.py
+++ imagetagger/imagetagger/tagger_messages/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'tagger_messages'
 urlpatterns = [
     url(r'^tagger_messages/send_message/team_message/$', views.send_team_message, name='send_team_message'),
+    url(r'^tagger_messages/send_message/global_message/$', views.send_global_message, name='send_global_message'),
 ]
