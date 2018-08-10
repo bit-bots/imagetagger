7547ce81a54081b31cdfda93cb63041f050e8213
Florian <florian@flova.de>
2018-08-10 19:45:20 +0200

    Improves message overview (fully working version)

--- imagetagger/imagetagger/tagger_messages/views.py
+++ imagetagger/imagetagger/tagger_messages/views.py
@@ -9,7 +9,10 @@ from django.db.models import Q
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.http import HttpResponseRedirect
+from imagetagger.tagger_messages.forms import TeamMessageCreationForm, GlobalMessageCreationForm
 from imagetagger.users.models import TeamMembership
+from imagetagger.users.models import User, Team
+from datetime import date, timedelta
 
 @login_required
 def send_team_message(request):
@@ -21,11 +24,11 @@ def send_team_message(request):
             with transaction.atomic():
                 form.instance.creator = request.user
                 form.instance.save()
-            return redirect(request.POST['source'])
+                form.instance.read_by.add(request.user)
+                form.instance.save()
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         messages.error(request, 'Invalid message form')
-    if 'source' in request.POST:
-        return redirect(request.POST['source'])
-    return redirect(reverse('images:index'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 @staff_member_required
 def send_global_message(request):
@@ -34,19 +37,24 @@ def send_global_message(request):
         if form.is_valid():
             with transaction.atomic():
                 form.instance.creator = request.user
-                print(form)
                 form.instance.save()
-            return redirect(request.POST['source'])
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         messages.error(request, 'Invalid message form')
-    if 'source' in request.POST:
-        return redirect(request.POST['source'])
-    return redirect(reverse('images:index'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 @login_required
 def read_message(request, message_id):
     message = Message.objects.get(id=message_id)
     message.read_by.add(request.user)
     message.save()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+@login_required
+def read_all_messages(request):
+    messages = Message.in_range(TeamMessage.get_messages_for_user(request.user)).filter(~Q(read_by=request.user))
+    current_user = User.objects.get(username=request.user.username)
+    current_user.read_messages.add(*messages)
+    current_user.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 @login_required
@@ -60,33 +68,73 @@ def delete_message(request, message_id):
 @login_required
 def overview_unread(request):
     usermessages = Message.in_range(TeamMessage.get_messages_for_user(request.user)).filter(~Q(read_by=request.user))
+    user_admin_teams = Team.objects.filter(memberships__user=request.user, memberships__is_admin=True)
+
+    team_message_creation_form = TeamMessageCreationForm(
+        initial={
+            'start_time': str(date.today()),
+            'expire_time': str(date.today() + timedelta(days=1)),
+        })
+    team_message_creation_form.fields['team'].queryset = user_admin_teams
 
     return TemplateResponse(request, 'tagger_messages/overview.html', {
         'mode': 'unread',
         'usermessages': usermessages,
+        'team_message_creation_form': team_message_creation_form,
+        'user_has_admin_teams': user_admin_teams.exists(),
     })
 
 @login_required
 def overview_all(request):
     usermessages = TeamMessage.get_messages_for_user(request.user)
+    user_admin_teams =  Team.objects.filter(memberships__user=request.user, memberships__is_admin=True)  
+
+    team_message_creation_form = TeamMessageCreationForm(
+        initial={
+            'start_time': str(date.today()),
+            'expire_time': str(date.today() + timedelta(days=1)),
+        })
+    team_message_creation_form.fields['team'].queryset = user_admin_teams
+
     return TemplateResponse(request, 'tagger_messages/overview.html', {
         'mode': 'all',
         'usermessages': usermessages,
+        'team_message_creation_form': team_message_creation_form,
+        'user_has_admin_teams': user_admin_teams.exists(),
     })
 
 @login_required
 def overview_sent(request):
     usermessages = TeamMessage.get_messages_for_user(request.user).filter(creator=request.user)
+    user_admin_teams = Team.objects.filter(memberships__user=request.user, memberships__is_admin=True)
+    
+    team_message_creation_form = TeamMessageCreationForm(
+        initial={
+            'start_time': str(date.today()),
+            'expire_time': str(date.today() + timedelta(days=1)),
+        })
+    team_message_creation_form.fields['team'].queryset = user_admin_teams
+
     return TemplateResponse(request, 'tagger_messages/overview.html', {
         'mode': 'sent',
         'usermessages': usermessages,
+        'team_message_creation_form': team_message_creation_form,
+        'user_has_admin_teams': user_admin_teams.exists(),
     })
 
 @login_required
 def overview_global(request):
     global_annoucements = GlobalMessage.get(request.user)
+
+    global_message_creation_form = GlobalMessageCreationForm(
+        initial={
+            'start_time': str(date.today()),
+            'expire_time': str(date.today() + timedelta(days=1)),
+        })
+
     return TemplateResponse(request, 'tagger_messages/overview.html', {
         'mode': 'global',
         'global_annoucements': global_annoucements,
         'user': request.user,
+        'global_message_creation_form': global_message_creation_form,
     })
