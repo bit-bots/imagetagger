67ff72493541c1baa5c73fe1bc9da6baa59c9bb6
Florian <florian@flova.de>
2018-08-10 23:18:53 +0200

    Does some refactoring to improve the code style

--- imagetagger/imagetagger/tagger_messages/views.py
+++ imagetagger/imagetagger/tagger_messages/views.py
@@ -1,18 +1,16 @@
-from django.urls import reverse
+from datetime import date, timedelta
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib import messages
 from django.db.models import Q
-from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.http import HttpResponseRedirect
 from django.db import transaction
-from imagetagger.tagger_messages.forms import TeamMessageCreationForm, GlobalMessageCreationForm
 from imagetagger.tagger_messages.models import Message, TeamMessage, GlobalMessage
 from imagetagger.tagger_messages.forms import TeamMessageCreationForm, GlobalMessageCreationForm
 from imagetagger.users.models import TeamMembership
 from imagetagger.users.models import User, Team
-from datetime import date, timedelta
+
 
 @login_required
 def send_team_message(request):
@@ -20,7 +18,7 @@ def send_team_message(request):
         form = TeamMessageCreationForm(request.POST)
         if (form.is_valid() and TeamMembership.objects.filter(
                 user=request.user, team=form.instance.team, is_admin=True
-                ).exists()):
+        ).exists()):
             with transaction.atomic():
                 form.instance.creator = request.user
                 form.instance.save()
@@ -29,6 +27,7 @@ def send_team_message(request):
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         messages.error(request, 'Invalid message form')
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 @staff_member_required
 def send_global_message(request):
@@ -42,12 +41,14 @@ def send_global_message(request):
         messages.error(request, 'Invalid message form')
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+
 @login_required
 def read_message(request, message_id):
     message = Message.objects.get(id=message_id)
     message.read_by.add(request.user)
     message.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 @login_required
 def read_all_messages(request):
@@ -57,6 +58,7 @@ def read_all_messages(request):
     current_user.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+
 @login_required
 def delete_message(request, message_id):
     if request.user.is_staff:
@@ -64,6 +66,7 @@ def delete_message(request, message_id):
     else:
         Message.objects.filter(id=message_id, creator=request.user).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 @login_required
 def overview_unread(request):
@@ -84,11 +87,12 @@ def overview_unread(request):
         'user_has_admin_teams': user_admin_teams.exists(),
     })
 
+
 @login_required
 def overview_all(request):
     # Gets all team messages for the user, even from the past and future
     usermessages = TeamMessage.get_messages_for_user(request.user)
-    user_admin_teams =  Team.objects.filter(memberships__user=request.user, memberships__is_admin=True)  
+    user_admin_teams = Team.objects.filter(memberships__user=request.user, memberships__is_admin=True)
 
     team_message_creation_form = TeamMessageCreationForm(
         initial={
@@ -104,12 +108,13 @@ def overview_all(request):
         'user_has_admin_teams': user_admin_teams.exists(),
     })
 
+
 @login_required
 def overview_sent(request):
     usermessages = TeamMessage.get_messages_for_user(request.user).filter(creator=request.user)
     # get all teams where the user is an admin
     user_admin_teams = Team.objects.filter(memberships__user=request.user, memberships__is_admin=True)
-    
+
     team_message_creation_form = TeamMessageCreationForm(
         initial={
             'start_time': str(date.today()),
@@ -123,6 +128,7 @@ def overview_sent(request):
         'team_message_creation_form': team_message_creation_form,
         'user_has_admin_teams': user_admin_teams.exists(),
     })
+
 
 @login_required
 def overview_global(request):
