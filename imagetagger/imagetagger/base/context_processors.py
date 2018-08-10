b3e9738b11c9d352ecd87c8d5a6239e4637d4985
Florian <florian@flova.de>
2018-08-10 16:11:48 +0200

    Adds red notification

--- imagetagger/imagetagger/base/context_processors.py
+++ imagetagger/imagetagger/base/context_processors.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from imagetagger.users.models import Team
+from imagetagger.tagger_messages.models import TeamMessage
+from django.db.models import Q
 
 
 def base_data(request):
     if request.user.is_authenticated:
         my_teams = Team.objects.filter(members=request.user)
+        unread_message_count = TeamMessage.get_messages_for_user(request.user).filter(~Q(read_by=request.user)).count()
     else:
         my_teams = None
 
@@ -14,4 +17,5 @@ def base_data(request):
         'IMPRINT_NAME': settings.IMPRINT_NAME,
         'TOOLS_ENABLED': settings.TOOLS_ENABLED,
         'my_teams': my_teams,
+        'unread_message_count': unread_message_count,
     }
