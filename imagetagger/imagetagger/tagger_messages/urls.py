254d104b60c37857a87e48ebda15006fa4f5694a
Florian <florian@flova.de>
2018-08-10 20:34:05 +0200

    Fixing bug in url names

--- imagetagger/imagetagger/tagger_messages/urls.py
+++ imagetagger/imagetagger/tagger_messages/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 app_name = 'tagger_messages'
 urlpatterns = [
-    url(r'^tagger_messages/send_message/team_message/$', views.send_team_message, name='send_team_message'),
-    url(r'^tagger_messages/send_message/global_message/$', views.send_global_message, name='send_global_message'),
-    url(r'^tagger_messages/read_message/(\d+)/$', views.read_message, name='read_message'),
-    url(r'^tagger_messages/read_message/all/$', views.read_all_messages, name='read_all_messages'),
-    url(r'^tagger_messages/delete_message/(\d+)/$', views.delete_message, name='delete_message'),
-    url(r'^tagger_messages/overview/$', views.overview_unread, name='overview'),
-    url(r'^tagger_messages/overview/unread/$', views.overview_unread, name='overview_unread'),
-    url(r'^tagger_messages/overview/all/$', views.overview_all, name='overview_all'),
-    url(r'^tagger_messages/overview/sent/$', views.overview_sent, name='overview_sent'),
-    url(r'^tagger_messages/overview/global/$', views.overview_global, name='overview_global'),
+    url(r'^send_message/team_message/$', views.send_team_message, name='send_team_message'),
+    url(r'^send_message/global_message/$', views.send_global_message, name='send_global_message'),
+    url(r'^read_message/(\d+)/$', views.read_message, name='read_message'),
+    url(r'^read_message/all/$', views.read_all_messages, name='read_all_messages'),
+    url(r'^delete_message/(\d+)/$', views.delete_message, name='delete_message'),
+    url(r'^overview/$', views.overview_unread, name='overview'),
+    url(r'^overview/unread/$', views.overview_unread, name='overview_unread'),
+    url(r'^overview/all/$', views.overview_all, name='overview_all'),
+    url(r'^overview/sent/$', views.overview_sent, name='overview_sent'),
+    url(r'^overview/global/$', views.overview_global, name='overview_global'),
 ]
