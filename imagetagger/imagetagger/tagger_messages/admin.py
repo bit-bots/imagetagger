a87a1c5f5ab21135486555e43ccc9bb278fdb5b2
Florian <florian@flova.de>
2018-11-14 16:37:31 +0100

    Adds taggermessages to the admin page.

--- imagetagger/imagetagger/tagger_messages/admin.py
+++ imagetagger/imagetagger/tagger_messages/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import TeamMessage, GlobalMessage
+
+admin.site.register(TeamMessage)
+admin.site.register(GlobalMessage)
