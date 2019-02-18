732e8e2b6b2362168d34163b364211726bd5e108
Timon Engelke <timon.engelke@online.de>
2019-02-18 12:21:02 +0100

    Allow SessionAuthentication

--- imagetagger/imagetagger/settings_base.py
+++ imagetagger/imagetagger/settings_base.py
@@ -146,6 +146,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
