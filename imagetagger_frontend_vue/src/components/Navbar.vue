2876cbb20bc5bbbd665a51f3002f78a1105e47c6
Finn-Thorben Sell <dev@finn-thorben.me>
2020-01-17 15:25:08 +0100

    add simple navbar

---
+++ imagetagger_frontend_vue/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <router-link class="navbar-item" :to="{ name: 'dashboard', params: {filter: 'all'} }">
+                <img src="../assets/bit-bot.png" alt="bit-bots logo">
+                <span>ImageTagger</span>
+            </router-link>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"/>
+                <span aria-hidden="true"/>
+                <span aria-hidden="true"/>
+            </a>
+        </div>
+
+        <div class="navbar-menu">
+            <div class="navbar-start"></div>
+            <div class="navbar-end">
+                <router-link to="/" class="navbar-item">Home</router-link>
+                <router-link to="/" class="navbar-item">Explore</router-link>
+                <router-link to="/" class="navbar-item">Messages</router-link>
+                <router-link to="/" class="navbar-item">Tools</router-link>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped lang="scss">
+    .navbar-brand img {
+        padding-right: 6px;
+    }
+</style>
--- imagetagger_frontend_vue/src/views/Dashboard.vue
+++ imagetagger_frontend_vue/src/views/Dashboard.vue
@@ -1,19 +1,26 @@
 <template>
-    <section class="section">
-        <div class="container">
-            <img src="../assets/bit-bot.png">
-            <h1 class="title">
-                Hello World
-            </h1>
-            <p class="subtitle">
-                My first website with <strong>{{test}}</strong>!
-            </p>
-        </div>
-    </section>
+    <div>
+        <Navbar/>
+
+        <section class="section">
+            <div class="container">
+                <img src="../assets/bit-bot.png">
+                <h1 class="title">
+                    Hello World
+                </h1>
+                <p class="subtitle">
+                    My first website with <strong>{{test}}</strong>!
+                </p>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
+    import Navbar from "components/Navbar";
+
     export default {
+        components: {Navbar},
         computed: {
             test() {
                 return this.$route.params
