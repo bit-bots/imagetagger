41e2b7c9c17211c15bbc6815c05e989bcd31ade3
Finn-Thorben Sell <dev@finn-thorben.me>
2020-02-07 16:53:12 +0100

    remove obsolete card component

--- imagetagger_frontend_vue/src/views/Dashboard.vue
+++ imagetagger_frontend_vue/src/views/Dashboard.vue
@@ -11,48 +11,6 @@
                 </div>
             </div>
         </div>
-
-        <!--
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col col-12"><h1>Dashboard</h1></div>
-            </div>
-
-            <div class="row">
-                <div class="col col-8">
-                    <Card class="my-2" title="Imagesets" :tabs="tabs" :active-tab-name="activeImagesetTab">
-                        <div>Public content</div>
-                    </Card>
-                </div>
-
-                <div class="col col-4">
-                    <div class="row">
-                        <div class="container-fluid">
-                            <Card class="my-2" title="Create Team">
-                                <div class="alert alert-warning">Create Team is not yet implemented</div>
-                            </Card>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="container-fluid">
-                            <Card class="my-2" title="Create Imageset">
-                                <div class="alert alert-warning">Create Imageset is not yet implemented</div>
-                            </Card>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="container-fluid">
-                            <Card class="my-2" title="Imagetagger Stats">
-                                <div class="alert alert-warning">Create Team is not yet implemented</div>
-                            </Card>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        -->
     </div>
 </template>
 
@@ -60,24 +18,12 @@
 import Vue from "vue"
 import Component from "vue-class-component"
 import Navbar from "@/components/Navbar.vue"
-import Card from "@/components/Card.vue"
-import {CardTabDefinition} from "@/types"
 import NavbarSearch from "@/components/NavbarSearch.vue"
 
 @Component({
-    components: {NavbarSearch, Card, Navbar}
+    components: {NavbarSearch, Navbar}
 })
 export default class Dashboard extends Vue {
-    get activeImagesetTab (): string {
-        return this.$route.params.filter
-    }
-
-    get tabs(): CardTabDefinition[] {
-        return [
-            new CardTabDefinition("All", {name: "dashboard", params: {filter: "all"}}),
-            new CardTabDefinition("Public", {name: "dashboard", params: {filter: "public"}})
-        ]
-    }
 }
 </script>
 
