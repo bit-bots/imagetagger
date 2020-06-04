368fe1d25a102fd8b02fb00064e16df7676761dc
Finn-Thorben Sell <dev@finn-thorben.me>
2020-06-04 21:37:04 +0200

    update rest-framework router registration calls

--- imagetagger/imagetagger/api/urls.py
+++ imagetagger/imagetagger/api/urls.py
@@ -7,16 +7,16 @@ from imagetagger.api import views
 app_name = 'api'
 
 router = DefaultRouter()
-router.register(r'annotations', views.AnnotationViewSet, base_name='annotations')
-router.register(r'annotation_types', views.AnnotationTypeViewSet, base_name='annotation_types')
-router.register(r'export_formats', views.ExportFormatViewSet, base_name='export_formats')
-router.register(r'exports', views.ExportViewSet, base_name='exports')
-router.register(r'image_sets', views.ImageSetViewSet, base_name='image_sets')
+router.register(r'annotations', views.AnnotationViewSet, basename='annotations')
+router.register(r'annotation_types', views.AnnotationTypeViewSet, basename='annotation_types')
+router.register(r'export_formats', views.ExportFormatViewSet, basename='export_formats')
+router.register(r'exports', views.ExportViewSet, basename='exports')
+router.register(r'image_sets', views.ImageSetViewSet, basename='image_sets')
 router.register(r'images', views.ImageViewSet)
-router.register(r'teams', views.TeamViewSet, base_name='teams')
-router.register(r'users', views.UserViewSet, base_name='users')
-router.register(r'verifications', views.VerificationViewSet, base_name='verifications')
-router.register(r'image_set_tags', views.ImagesetTagViewSet, base_name='image_set_tags')
+router.register(r'teams', views.TeamViewSet, basename='teams')
+router.register(r'users', views.UserViewSet, basename='users')
+router.register(r'verifications', views.VerificationViewSet, basename='verifications')
+router.register(r'image_set_tags', views.ImagesetTagViewSet, basename='image_set_tags')
 
 urlpatterns = [
     *router.urls,
