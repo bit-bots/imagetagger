174b7eba7fe786f2bfcdfb5b0c79aa449aa4b897
Finn-Thorben Sell <dev@finn-thorben.me>
2020-09-28 15:35:28 +0200

    update dependencies

--- imagetagger_frontend_vue/src/App.vue
+++ imagetagger_frontend_vue/src/App.vue
@@ -1,9 +1,9 @@
 <template>
     <v-app>
         <it-navbar/>
-        <v-content>
+        <v-main>
             <router-view/>
-        </v-content>
+        </v-main>
     </v-app>
 </template>
 
